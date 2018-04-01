bf890f4c5a67d12af79d26a5563b370dae69efc4
Esref Ozdemir <esref.ozdemir27@gmail.com>
2018-04-01 10:54:48 +0300

    build script

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Build the project.
+#
+# usage: ./build.sh <BUILD_TYPE> [<CMAKE_ARGS>...]
+#
+# <BUILD_TYPE> must be debug, release, clean.
+# <CMAKE_ARGS> are arguments to forward to cmake. You can use any cmake argument
+# here such as -DCMAKE_CXX_COMPILER=g++-5.
+#
+# example usages:
+# 
+# 1. Build only the library
+#
+#        ./build.sh release
+#
+# If no build type is given, the library is built in Release mode.
+
+build=$1
+shift
+
+if [[ ${build} == "debug" ]]; then
+	build_flag="-DCMAKE_BUILD_TYPE=Debug"
+elif [[ ${build} == "release" ]]; then
+	build_flag="-DCMAKE_BUILD_TYPE=Release"
+elif [[ ${build} == "clean" ]]; then
+	rm -rf build ||:
+	rm -rf construct_datasets ||:
+	rm -rf classifier ||:
+	exit
+else
+	echo "Unknown build type: Use one of debug, release, clean"
+	exit
+fi
+
+mkdir -p build
+cd build
+cmake ${build_flag} $* ..
+make -j6
